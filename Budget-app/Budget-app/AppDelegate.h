9668d67808e69222fce0c100a33e98992093b78a
tyson <tyson@labs.hiddengeniusproject.org>
2012-07-11 14:44:58 -0700

    Second Commit

---
+++ Budget-app/Budget-app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Budget-app
+//
+//  Created by Tyson Walton on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Budget-app/Budget-app/ViewController.h
@@ -0,0 +1,56 @@
+//
+//  ViewController.h
+//  Budget-app
+//
+//  Created by Tyson Walton on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+@property (nonatomic, retain) IBOutlet UITextField *inputText;
+@property (nonatomic, retain) IBOutlet UITextField *inputText;
+@property (nonatomic, retain) IBOutlet UITextField *inputText;
+@property (nonatomic, retain) IBOutlet UITextField *inputText;
+@property (nonatomic, retain) IBOutlet UITextField *inputText;
+@property (nonatomic, retain) IBOutlet UITextField *inputText;
+@property (nonatomic, retain) IBOutlet UITextField *inputText;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
